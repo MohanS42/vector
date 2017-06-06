c4b0e0e5303a6b74e85a21ced975caca0030b21b
MohanS42 <mohumms007@gmail.com>
2017-06-06 13:43:37 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int g;
+    printf("enter the number:");
+    scanf("%d",&g);
+    if(g%2==0)
+    printf("even %d",g);
+    else
+    printf("odd %d",g);
+    return 0;
+}
