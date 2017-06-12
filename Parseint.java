89a09b50a9189bcea6d861e892e8ca6222f91b51
MohanS42 <mohumms007@gmail.com>
2017-06-12 16:01:07 +0530

    Add files via upload

---
+++ Parseint.java
@@ -0,0 +1,11 @@
+package Guvi;
+
+public class Parseint {
+public static void main(String[] args)
+{
+	String ch="300";
+int	i=Integer.parseInt(ch);
+	System.out.println(ch + 200);
+	System.out.println(i + 200);
+}
+}
