f9e36a5b1e054147ba6c6520cbe64477c2024893
MohanS42 <mohumms007@gmail.com>
2017-06-12 15:51:55 +0530

    Add files via upload

---
+++ RverseString.java
@@ -0,0 +1,12 @@
+package Guvi;
+
+public class RverseString {
+
+	public static void main(String[] args) {
+		String string="abcd";
+		String reverse=new StringBuffer(string).reverse().toString();
+	System.out.println(" Before " + string);
+	System.out.println(" After  " + reverse);
+	}
+
+}
