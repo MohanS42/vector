d4d1f9e509f2ada3a6f12dc7e003f4d9c832115c
MohanS42 <mohumms007@gmail.com>
2017-06-12 15:31:29 +0530

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,17 @@
+package Guvi;
+
+import java.util.Scanner;
+
+public class Fact {
+
+	public static void main(String[] args) {
+		int i,num,fact=1;
+		Scanner sc=new Scanner(System.in); 
+		System.out.println("Enter the number");
+		num=sc.nextInt();
+		for(i=1;i<=num;i++){
+			fact=fact*i;
+	}
+System.out.println("factorial is "+ fact);
+	}
+}
